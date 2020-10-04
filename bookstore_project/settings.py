48afaba874d9f783c37753bd6ad46369c6d6f6b0
j <longnguyen16.ck27@st.ueh.edu.vn>
2020-10-05 00:01:26 +0700

    Chap 6 Set up static files

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -41,7 +41,12 @@ INSTALLED_APPS = [
     # Local
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
+
+    # 3rd party
+    'crispy_forms',
 ]
+# django-crispy-forms 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -126,6 +131,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_FINDERS = [ 
+    "django.contrib.staticfiles.finders.FileSystemFinder", 
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
