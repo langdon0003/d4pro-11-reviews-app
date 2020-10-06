b90e20740b31ec1932efa2ab73e81a7165b045f4
j <longnguyen16.ck27@st.ueh.edu.vn>
2020-10-06 20:35:50 +0700

    Done Chap 11 Setup Reviews App

--- books/models.py
+++ books/models.py
@@ -1,7 +1,7 @@
-from django.db import models
 import uuid
-
+from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 class Book(models.Model):
   title = models.CharField(max_length=200)
@@ -17,6 +17,19 @@ class Book(models.Model):
   def __str__(self):
     return self.title
 
-  
   def get_absolute_url(self):
     return reverse('book_detail', args=[str(self.id)])
+
+class Review(models.Model):
+  book = models.ForeignKey(
+    Book,
+    on_delete=models.CASCADE,
+    related_name="reviews"
+  )
+  review = models.TextField(max_length=100)
+  author = models.ForeignKey(
+    get_user_model(),
+    on_delete=models.CASCADE
+  )
+  def __str__(self):
+    return self.review
